ce12a17362cf4e6892ade435608fd01cb22bb6fd
ivy <ivynghuyxingivy@gmail.com>
2026-03-22 20:40:51 +0800

    update

---
+++ app/_header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+    <meta charset ="UFT-8">
+    <meta name="viewport" content= "width=device-width, initial-scale=1.0">
+    <title><?= $title ?? 'Untitled' ?></title>
+    <link rel = "shortcut icon" href="/images/favicon.png">
+    <link rel = "stylesheet" href="/css/app.css">
+</head>
+<body>
+    <header>
+        <h1><a href="/">Stationary Hub</a></h1>
+    </header>
+
+    <nav>
+        <a href="/"><b>Home</b></a>
+    </nav>
+
+    <main>
+        <h1><?= $_title ?? 'Untitled' ?></h1>

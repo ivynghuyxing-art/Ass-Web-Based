af7e193d774bfad68dc7326924444a060f1967cf
ivy <ivynghuyxingivy@gmail.com>
2026-03-29 02:03:14 +0800

    change

--- app/customer_header.php
+++ app/customer_header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content= "width=device-width, initial-scale=1.0">
     <title><?= $title ?? 'Untitled' ?></title>
     <link rel = "shortcut icon" href="/images/favicon.png">
-    <link rel = "stylesheet" href="/css/app.css">
+    <link rel = "stylesheet" href="/css/user.css">
 </head>
 <body>
     <header>

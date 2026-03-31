e55ac113dff8ea2774e64d1cdeeb2886e4dc5f4d
ivy <ivynghuyxingivy@gmail.com>
2026-03-31 21:13:47 +0800

    update

--- app/register.php
+++ app/register.php
@@ -68,7 +68,7 @@ if(is_post()){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register - Cozy Hub</title>
+    <title>Register | Cozy Hub</title>
     <link rel="shortcut icon" href="/images/favicon.png">
     <link rel="stylesheet" href="/css/app.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
@@ -101,7 +101,7 @@ if(is_post()){
                 <?= err('confirm') ?>
  
                 <label for="photo">Profile Photo</label>
-                <label class="upload" tabindex="0">
+                <label class="upload">
                     <?= html_file('photo', 'image/*', 'hidden') ?>
                     <img src="/images/photo.jpg">
                 </label>

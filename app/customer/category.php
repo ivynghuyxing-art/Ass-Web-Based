ceaee7a1fd32941fcf3e46365d7d5b04d2d9d4de
cayenne22 <tangcayenne@gmail.com>
2026-04-05 02:51:23 +0800

    hide search bar on category page

--- app/customer/category.php
+++ app/customer/category.php
@@ -1,7 +1,48 @@
 <?php
 $title = 'Category';
+$_title = 'Category';
 include '../customer_header.php';
 ?>
+
+<!-- Search Bar Design -->
+ <style>
+    .center-search {
+        text-align: center;
+        margin: 30px 0;
+    }
+
+    .center-search input {
+        width: 400px;
+        padding: 12px;
+        font-size: 16px;
+        border: 1px solid #ccc;
+        border-radius: 6px;
+    }
+
+    .center-search button {
+        padding: 12px 20px;
+        font-size: 16px;
+        margin-left: 10px;
+        background-color: #333;
+        color: #fff;
+        border: none;
+        border-radius: 6px;
+        cursor: pointer;
+    }
+
+    .center-search button:hover {
+        background-color: #555;
+    }
+
+</style>
+
+<!-- ===== Search Bar ===== -->
+<div class="center-search">
+    <form action="/customer/search.php" method="get">
+        <input type="search" name="product_name" placeholder="Search product">
+        <button type="submit">Search</button>
+    </form>
+</div>
 
 <?php
 include '../_foot.php';
--- app/customer_header.php
+++ app/customer_header.php
@@ -50,11 +50,13 @@ require'_base.php';
         </div> 
 
         <div class="right">
-            <div class ="search-bar">
+            <?php if ($_title !== 'Category'): ?>
+             <div class ="search-bar">
                 <form action="customer/search.php" method="get" class="search-form">
                     <input type="search" id="search"name="product_name" placeholder="Search product">
                 </form>        
-            </div>
+             </div>
+            <?php endif; ?>
 
             <div class="cart-btn">
             <?php

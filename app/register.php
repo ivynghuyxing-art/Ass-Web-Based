b46386cbfa538268f348e97804ee065f1ee10001
ivy <ivynghuyxingivy@gmail.com>
2026-04-09 21:31:05 +0800

    update

--- app/register.php
+++ app/register.php
@@ -92,7 +92,7 @@ $title = 'Register | Cozy Hub';
 <head>
     <meta charset ="UTF-8">
     <meta name="viewport" content= "width=device-width, initial-scale=1.0">
-    <title><?= $title ?? 'Untitled' ?></title>+
+    <title><?= $title ?? 'Untitled' ?></title>
     <link rel = "shortcut icon" href="/images/favicon.png">
     <link rel = "stylesheet" href="/css/app.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
